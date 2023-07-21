3211784ccc36fbef3daf434e2d23526580d6ec2e
Ahmed-M-Aboutaleb <iifire005@gmail.com>
2023-07-21 23:29:56 +0300

    Done Security

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,15 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { Logger, ValidationPipe } from '@nestjs/common';
+import helmet from 'helmet';
 
 const PORT = parseInt(process.env.SERVER_PORT);
 const VERSION = 'v1';
 
 async function bootstrap() {
     const app = await NestFactory.create(AppModule);
+    app.use(helmet());
+    app.enableCors();
     app.useGlobalPipes(new ValidationPipe());
     app.setGlobalPrefix(VERSION);
     await app.listen(PORT);
