d1904710491e0ae9e1e38fd9e8c6b9ae99b71fd9
Ahmed-M-Aboutaleb <iifire005@gmail.com>
2023-07-22 17:25:47 +0300

    User done and product update

--- src/product/product.module.ts
+++ src/product/product.module.ts
@@ -5,10 +5,14 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { ProductSchema } from './product.schema';
 import { JwtModule } from '@nestjs/jwt';
 import { ConfigModule, ConfigService } from '@nestjs/config';
+import { CategorySchema } from 'src/category/category.schema';
 
 @Module({
     imports: [
-        MongooseModule.forFeature([{ name: 'Product', schema: ProductSchema }]),
+        MongooseModule.forFeature([
+            { name: 'Product', schema: ProductSchema },
+            { name: 'Category', schema: CategorySchema },
+        ]),
         JwtModule.registerAsync({
             imports: [ConfigModule],
             useFactory: async (configService: ConfigService) => ({
--- src/product/product.service.ts
+++ src/product/product.service.ts
@@ -1,4 +1,8 @@
-import { Injectable, UnauthorizedException } from '@nestjs/common';
+import {
+    Injectable,
+    UnauthorizedException,
+    BadRequestException,
+} from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { v4 as uuidv4 } from 'uuid';
 import { Model } from 'mongoose';
@@ -6,11 +10,13 @@ import { Product } from './product.schema';
 import ProductDto from './dto/product.dto';
 import CreateProductDto from './dto/create-product.dto';
 import { Role } from 'src/roles/role.enum';
+import { Category } from 'src/category/category.schema';
 
 @Injectable()
 export class ProductService {
     constructor(
         @InjectModel(Product.name) private productModel: Model<Product>,
+        @InjectModel(Category.name) private categoryModel: Model<Category>,
     ) {}
 
     async find(page: number): Promise<ProductDto[]> {
@@ -31,6 +37,14 @@ export class ProductService {
         product.id = await uuidv4();
         product.numReviews = 0;
         product.rating = 0;
+        const category = product.category;
+        console.log(category);
+        const categoryDB = await this.categoryModel
+            .find({ name: category })
+            .exec();
+        if (categoryDB.length == 0) {
+            throw new BadRequestException('Category not found');
+        }
         const newProduct = new this.productModel(product);
         return await newProduct.save();
     }
@@ -48,6 +62,13 @@ export class ProductService {
         } else {
             this.findOne(id).then(async (oldProduct) => {
                 if (oldProduct.seller == seller) {
+                    const category = product.category;
+                    const categoryDB = await this.categoryModel
+                        .find({ name: category })
+                        .exec();
+                    if (categoryDB.length == 0) {
+                        throw new BadRequestException('Category not found');
+                    }
                     return await this.productModel.findByIdAndUpdate(
                         id,
                         product,
